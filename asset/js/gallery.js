3540568f33e814a23a4862d758ed8601a9e413f8
Abiy Menberu <IT.Abiy.Menberu19@gmail.com>
2021-02-14 02:34:57 +0300

    merged lab7 with the clock files, edited the navbar links, added a gallery carousel with lorem picsum images

---
+++ asset/js/gallery.js
@@ -0,0 +1,17 @@
+
+// var btns = document.querySelector('img')
+
+// btns.addEventListener('click', urlSwap())
+
+// function urlSwap() {
+//     btns.src = "https://picsum.photos/1920/1080"
+//     console.log("swapped")
+// }
+
+// setInterval (imageSwap, 5000);
+
+
+// function imageSwap() {
+//     document.querySelector('img').src = 'https://picsum.photos/1920/1080?blur2'
+//     // document.getElementById('load-anim').style.display = 'none'
+// }
---
+++ asset/js/load.js
@@ -0,0 +1,10 @@
+setTimeout(myFunction, 2000);
+
+
+document.getElementById('blogs').style.display = 'none'
+
+function myFunction() {
+    document.getElementById('blogs').style.display = 'block'
+    document.getElementById('load-anim').style.display = 'none'
+}
+
